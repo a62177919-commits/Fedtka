feb5fc9dba33452299429c1513c23ad9192d3b23
a62177919-commits <a62177919@gmail.com>
2026-01-18 02:40:06 +0400

    Update Discord token and modify response messages

--- api/get.js
+++ api/get.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    // Вставляем токен и ID прямо в код для надежности
-    const TOKEN = 'MTM5NTA2MjEyNzY3NjM1ODc0Nw.GnFr1i.yytqQipHIZ3Wm1Z1e0gmA9XO_MvS8ZzpdS-smY';
+    // Твой ОБНОВЛЕННЫЙ токен
+    const TOKEN = 'MTM5NTA2MjEyNzY3NjM1ODc0Nw.G4PDRG.yjH51pQDZKnrx52t8Uzzx4GaSz7FsnzMWck8YA';
     const CHANNEL_ID = '1401775061706346536';
 
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,13 +13,11 @@ module.exports = async (req, res) => {
         });
 
         if (response.data && response.data.length > 0) {
-            // Отправляем только текст сообщения
             res.status(200).send(response.data[0].content);
         } else {
-            res.status(200).send("Сообщений в канале пока нет.");
+            res.status(200).send("Сообщений нет");
         }
     } catch (err) {
-        // Если ошибка, выводим её текст
         const errorMsg = err.response?.data?.message || err.message;
         res.status(200).send("Ошибка Discord: " + errorMsg);
     }
